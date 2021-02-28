18709b1deb3ca5ed63830c576096fef96dea8eb2
Danielle Silfa <d.silfa@ufl.edu>
2021-02-27 23:23:47 -0500

    About me change

--- src/About.js
+++ src/About.js
@@ -1,11 +1,11 @@
 const About = () => {
     return (
-        <div className="Home">
+    <div className="Home">
     <h1> About Me </h1>
-    <div class="space-x-60">
-    <p1 class="text-lg font-semibold text-gray-500"> Hi! My name is Danielle. I am a second-year CpE major at the University of Florida. </p1>
-        <img class="w-32 h-32 md:w-48 md:h-auto object-right float-right px-8" src="https://rehrealestate.com/wp-content/uploads/2015/08/facebook-default-no-profile-pic-girl.jpg"/>
-    </div>
+        <div class="space-x-60 grid-cols-2">
+            <p1 class="text-lg font-semibold text-gray-500"> Hi! My name is Danielle. I am a second-year CpE major at the University of Florida. </p1>
+            <img class="w-32 h-32 md:w-90 md:h-auto object-right" src="https://rehrealestate.com/wp-content/uploads/2015/08/facebook-default-no-profile-pic-girl.jpg"/>
+        </div>
     </div>
     );
 }
