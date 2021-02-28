d875d378db9588b59cf55bc41832620168645be6
katterillo <kattrina.erillo@gmail.com>
2021-02-27 19:57:50 -0500

    added elements to projects

---
+++ src/Projects.js
@@ -0,0 +1,9 @@
+const Projects = () => {
+    return (
+        <div class="border border-2 ">
+        <h1 class="text-center">Portfolio</h1>
+            <img src="https://picsum.photos/id/237/200/300"/>
+        </div>
+    );
+}
+export default Projects;
