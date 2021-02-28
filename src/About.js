666837b6b68e2009394c241fb82f11401ba9c718
katterillo <kattrina.erillo@gmail.com>
2021-02-27 23:40:10 -0500

    adjusted picture in about

--- src/About.js
+++ src/About.js
@@ -2,9 +2,9 @@ const About = () => {
     return (
     <div className="Home">
     <h1> About Me </h1>
-        <div class="space-x-60 grid-cols-2">
+        <div class="flex flex-wrap">
             <p1 class="text-lg font-semibold text-gray-500"> Hi! My name is Danielle. I am a second-year CpE major at the University of Florida. </p1>
-            <img class="w-32 h-32 md:w-90 md:h-auto object-right" src="https://rehrealestate.com/wp-content/uploads/2015/08/facebook-default-no-profile-pic-girl.jpg"/>
+            <img class="w-32 h-32 md:w-90 md:h-auto" src="https://rehrealestate.com/wp-content/uploads/2015/08/facebook-default-no-profile-pic-girl.jpg"/>
         </div>
     </div>
     );
