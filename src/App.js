3296d6a20252cc924c0f468b0d47c1751f7e6298
katterillo <kattrina.erillo@gmail.com>
2021-02-27 19:28:57 -0500

    added projects component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import About from './About.js';
+import Projects from './Projects.js';
+
 
 const App = () => {
   return (
     <div className="App">
       <h1> Hello World </h1>
       <About/>
+      <Projects/>
     </div>
   );
 }
