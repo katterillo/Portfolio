84e845f612c3abe46bec85b2045f3702973b4338
Danielle Silfa <d.silfa@ufl.edu>
2021-02-28 18:54:00 -0500

    Last Minute Nav Bar changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ const NavBar = () => {
     return (
     <div className="Navigation Bar">
        <Link to='/'><h1 class="text-blue-500 text-center text-7xl font-sans m-6"> Danielle Silfa </h1></Link>
-        <div class="grid grid-cols-4 gap-4 text-center bg-gray-200 divide-x-4 divide-gray-300 divide-double"> 
+        <div class="grid grid-cols-4 gap-4 text-center bg-gray-200 divide-x-4 divide-gray-300 divide-double pt-1 pb-1"> 
             <Link to='/'> <div class="font-medium hover:bg-gray-300 rounded">Home</div></Link>
             <Link to='/Projects'> <div class="font-medium hover:bg-gray-300 rounded">Projects</div></Link>
             <Link to='/Resume'><div class="font-medium hover:bg-gray-300 rounded">Resume</div></Link>
